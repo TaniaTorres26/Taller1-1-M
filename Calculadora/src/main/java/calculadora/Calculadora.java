aa4884784a84c3ad8c15f828e86921af515adf26
TaniaTorres26 <141367224+TaniaTorres26@users.noreply.github.com>
2023-08-10 14:29:21 -0500

    Add files via upload
    Ejercicio clase

---
+++ Calculadora/src/main/java/calculadora/Calculadora.java
@@ -0,0 +1,53 @@
+package calculadora;
+import java.util.Scanner;
+/*Tania Sofia Torres Romero*/
+public class Calculadora {
+
+  public static void main(String[] args) {
+    Scanner leer = new Scanner(System.in);
+    int opcion;
+    float num1 = 0;
+    float num2 = 0;
+    float res;
+
+    do{
+    System.out.println("Elige la operacion a realizar\n1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n5. Salir");
+    opcion = leer.nextInt();
+        switch (opcion) {
+    case 1 -> {
+      res = num1 + num2;
+      System.out.println("La suma de los numeros es: " + res);
+      break;
+    }
+    case 2 -> {
+      res = num1 - num2;
+      System.out.println("La resta de los numeros es: " + res);
+      break;
+    }
+    case 3 -> {
+      res = num1 * num2;
+      System.out.println("La multiplicacion de los numeros es: " + res);
+      break;
+    }
+    case 4 -> {
+      if (num2 == 0) {
+        System.out.println("Esta respuesta es indefinida");
+      } else {
+        res = num1 / num2;
+        System.out.println("La division de los numeros es: " + res);
+      }
+      break;
+    }
+    case 5 -> {
+      continue;
+    }
+    }
+
+    System.out.println("Digite el primer numero");
+    num1 = leer.nextInt();
+    System.out.println("Digite el segundo numero");
+    num2 = leer.nextInt();
+} while (opcion!=5);
+    
+}
+}
